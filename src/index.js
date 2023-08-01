4f9741431a08582ef8247e1be1ae8928a80319e7
Viktoriya Nykytenko <kitakiv@gmail.com>
2023-08-01 10:25:22 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let index = 0;
+  let result = '';
+  str = n.toString();
+  while (str.length > index) {
+    if (str[index] !== '-' && str[str - 1] !== '0') {
+        result = `${str[index]}${result}`
+    }
+    index ++;
+  }
+  return parseInt(result);
 }
